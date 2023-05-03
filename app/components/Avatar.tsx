e954d442a9bc48d0a71a2a382f6a1c7dea170978
Honeylyn Jimenez <honen.jimenez@gmail.com>
2023-05-03 02:22:15 -0600

    navigation bar

---
+++ app/components/Avatar.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+const Avatar = () => {
+    const router = useRouter ();
+    return (
+        <Image alt="Avatar" className="rounded-full" height="30"  width="30" src="/images/placeholder.jpg" />
+    )
+}
+
+export default Avatar;
